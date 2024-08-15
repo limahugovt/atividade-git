323ece279bcb30d5f0aeb62c3cc5c82aa5978901
limahugovt <lima.hugo@escolar.ifrn.edu.br>
2024-08-15 19:26:44 -0300

    feat: adding second route

--- curso/urls.py
+++ curso/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import home
+from .views import home, home2
 
 urlpatterns = [
     path('', home, name='home'),
+    path('home2/', home2, name='home2'),
 ]
--- curso/views.py
+++ curso/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def home2(request):
+    return render(request, 'index2.html')
